60a3dd33b43081ba987eda6fc3e6edb1df1b5310
joonhwan <joonhwan.lee@gmail.com>
2022-06-13 22:13:50 +0900

    untrack wwwwroot/dist/*

--- DevExtremeTemplate/DevExtremeWebPackDemo1/wwwroot/dist/index.bundle.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*******************************!*\
-  !*** ./src/js/pages/index.js ***!
-  \*******************************/
-console.log('index.js 가 로딩되었어요')
-/******/ })()
-;
-//# sourceMappingURL=index.bundle.js.map
